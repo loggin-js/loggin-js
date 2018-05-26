d04a2d381cb490782d63abd0f052eaf52a1c22f9
manoloedge96@gmail.com <manoloedge96@gmail.com>
2018-05-26 14:39:13 +0200

    added linting

--- scripts/run-tests.bash
+++ scripts/run-tests.bash
@@ -10,8 +10,13 @@ cd "${0%/*}/.."
 
 # let's fake failing test for now 
 echo "Running tests"
-echo "............................" 
-echo "Failed!" && exit 1
+# echo "............................" 
+npm test
+
+echo "Linting"
+npm run lint
+
+# echo "Failed!" && exit 1
 
 # example of commands for different languages
 # eslint .         # JS code quality check
