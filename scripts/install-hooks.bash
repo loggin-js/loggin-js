bb89c2c1d35e7ecca3ff9d50f561af8dd77741da
manoloedge96@gmail.com <manoloedge96@gmail.com>
2018-05-26 21:30:45 +0200

    updated install-hooks command

--- scripts/install-hooks.bash
+++ scripts/install-hooks.bash
@@ -5,9 +5,9 @@ GIT_DIR=$(git rev-parse --git-dir)
 echo "Installing hooks..."
 # this command creates symlink to our pre-commit script
 if [ -f $GIT_DIR/hooks/pre-commit ]; then
-    echo "pre-commit Already exists, removing it..."
+    echo "pre-commit already exists, removing it..."
     rm $GIT_DIR/hooks/pre-commit
 fi
 
 ln -s ../../scripts/pre-commit.bash $GIT_DIR/hooks/pre-commit
-echo "Installed hooks OK!"
+printf "Installed hooks \033[1;32mOK!"
