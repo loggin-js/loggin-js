e380730553369054fc6cadafd6ecd0c40792f123
manoloedge96@gmail.com <manoloedge96@gmail.com>
2018-07-10 23:31:39 +0200

    added output image

--- examples/basic-example.js
+++ examples/basic-example.js
@@ -30,16 +30,17 @@ logger.setUser('root');
 
 // Available predefined log levels
 logger.info('info', {
-  user: 'pedro',
-  id: 10
+  user: 'Jeffrey',
+  id: 101
 });
-logger.error('error');
-logger.warning('warning');
-logger.alert('alert');
-logger.emergency('emergency');
-logger.critical('critical', { dasd: '' });
-logger.debug('debug');
-logger.notice(['notice', 'notice']);
+logger.error('Teaching a snake to kick! :D');
+logger.warning('Whats that');
+logger.alert('Rice done.');
+logger.debug('Wow I can log stuff');
+logger.emergency('Lemons');
+logger.critical('critical', { error: 'S*** something is bad!' });
+logger.setUser('keff');
+logger.notice('notice im now an other user');
 logger.error('There was an <%rERROR>');
 
 // If enabled set to false logs will not be output
