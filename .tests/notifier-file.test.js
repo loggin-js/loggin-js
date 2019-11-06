3a2e313bb547c90f27bfefe905e8958e7cee39f8
nombrekeff <manoloedge96@gmail.com>
2019-11-06 22:37:07 +0100

    tests(add): Added new test for file notifier

---
+++ .tests/notifier-file.test.js
@@ -0,0 +1,33 @@
+/**
+* @jest-environment node
+*/
+let loggin = require('..');
+let fs = require('fs');
+let filePath = './logs/test-file.log';
+
+describe('loggin.Notifier.File tests', () => {
+    it(`should be registered`, () => {
+        expect(loggin.Notifier.File).toBeDefined();
+    });
+
+    it(`should construct notifier correctly`, () => {
+        expect(() => {
+            let notif = new loggin.Notifier.File();
+        }).not.toThrow();
+    });
+
+    beforeEach(() => {
+        if (fs.existsSync(filePath)) fs.unlinkSync(filePath);
+    });
+
+    it(`should log correctly to file`, () => {
+        let notif = loggin.notifier('file');
+        notif.pipe(loggin.severity('debug'), filePath);
+
+        let log = new loggin.Log('Test', null, loggin.severity('DEBUG'));
+        notif.output(log.message, log);
+
+        const fileData = fs.readFileSync(filePath).toString().trim();
+        expect(fileData).toEqual('Test');
+    });
+});
