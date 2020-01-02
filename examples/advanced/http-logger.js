425233c42067f2b0c3babdbe38cbd97e27e7b420
nombrekeff <manoloedge96@gmail.com>
2020-01-02 14:03:31 +0100

    fix(AdditionalNotifiers): added correct names to notifiers

--- examples/advanced/http-logger.js
+++ examples/advanced/http-logger.js
@@ -1,5 +1,5 @@
 const loggin = require('../..');
-let logger = loggin.logger('http', {
+let logger = loggin.logger('remote', {
     host: 'webhook.site',
     path: '2f4e9187-8ce7-4b3a-9a17-7af8206742d2',
     protocol: 'http'
