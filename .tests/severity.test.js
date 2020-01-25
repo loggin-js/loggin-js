4db62a619192692bf1836281b762fc6ae2c034f9
nombrekeff <manoloedge96@gmail.com>
2020-01-25 20:57:48 +0100

    added some tests

--- .tests/severity.test.js
+++ .tests/severity.test.js
@@ -7,5 +7,15 @@ describe('loggin.Severity', () => {
     it(`Class should be defined`, () => {
         expect(loggin.Severity).toBeDefined();
     });
-    // TODO: Add tests
+
+    it(`get debug severity works`, () => {
+        expect(loggin.severity('debug')).toBeTruthy();
+    });
+
+    it(`test value filtering (debug.canLog(info))`, () => {
+        let debug = loggin.severity('debug');
+        let info = loggin.severity('info');
+        expect(debug.canLog(info)).toBe(true);
+        expect(info.canLog(debug)).toBe(false);
+    });
 });
