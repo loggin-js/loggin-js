30deee3cc156139d000b8f96a136648eb94f706c
nombrekeff <manoloedge96@gmail.com>
2021-06-20 22:31:27 +0200

    chore: added eslint with some basic rules

--- src/lib/formatter.js
+++ src/lib/formatter.js
@@ -1,48 +1,46 @@
 'use strict';
-const EmptyRegistry = require('./registry/empty-registry');
-const colors = require('colors');
 
+const colors = require('colors');
 const strif = require('strif');
+const EmptyRegistry = require('./registry/empty-registry');
 const { defaultTransformers } = require('./transformers');
+const { throwIf } = require('./utils/type-checks');
 
 const strifFormatter = strif.create({ transformers: defaultTransformers });
 
 class Formatter {
-  constructor(template) {
-    this.template = template;
-  }
+	constructor(template) {
+		this.template = template;
+	}
 
-  formatLog(log, { color = false } = {}) {
-    return Formatter.format(log, this, color);
-  }
+	formatLog(log, { color = false } = {}) {
+		return Formatter.format(log, this, color);
+	}
 
-  static format(log, formatter, color = false) {
-    if (!log || !formatter) {
-      throw Error('"log" and "formatter" parameters are required');
-    }
-    if (!(formatter instanceof Formatter)) {
-      throw Error('"formatter" must be a Formatter instance');
-    }
-    if (!(formatter.template instanceof strif.Template)) {
-      throw Error(`"formatter" should be type: "StrifTemplate", not: ${typeof (formatter.template)}`);
-    }
+	static format(log, formatter, color = false) {
+		if (!log || !formatter) {
+			throw Error('"log" and "formatter" parameters are required');
+		}
+		if (!(formatter instanceof Formatter)) {
+			throw Error('"formatter" must be a Formatter instance');
+		}
 
-    if (color) {
-      colors.enable();
-    } else {
-      colors.disable();
-    }
+		if (color) {
+			colors.enable();
+		} else {
+			colors.disable();
+		}
 
-    return formatter.template.compile(log);
-  }
+		return formatter.template.compile(log);
+	}
 
-  static create(template, options = {}) {
-    if (typeof template !== 'string') {
-      throw new Error(`"template" must be a string got: ${typeof (template)}`);
-    }
+	static create(template, options = {}) {
+		if (typeof template !== 'string') {
+			throw new Error(`"template" must be a string got: ${typeof (template)}`);
+		}
 
-    return new Formatter(strifFormatter.template(template, options));
-  }
+		return new Formatter(strifFormatter.template(template, options));
+	}
 }
 
 Formatter.registry = new EmptyRegistry();
